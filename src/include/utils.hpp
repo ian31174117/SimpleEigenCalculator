4d529be0fb927edccbc21c5712aeeab7dfc394a4
ian31174117 <ian31174117@gmail.com>
2024-12-15 14:22:37 +0800

    git cache removed

--- src/include/utils.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include<iostream>
-#include "matrix.hpp"
-
-Matrix multiply_naive(const Matrix &A, const Matrix &B);
--- src/lib/matrix.cpp
+++
@@ -1,8 +0,0 @@
-#include "matrix.hpp"
-
-Matrix::Matrix(size_t _rows, size_t _cols) {
-    this->rows = _rows;
-    this->cols = _cols;
-    this->data = new double[this->rows * this->cols];
-    std::fill(this->data, this->data + this->rows * this->cols, 0.0);
-}
